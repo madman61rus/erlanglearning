de87c067837e337aec9b8eb3d4e52bc4a0858f41
Vladimir Kompaniec <vkompaniec@gmail.com>
2015-11-20 11:19:01 +0300

    Compare is ended

--- compare.erl
+++ compare.erl
@@ -21,20 +21,16 @@ reverse (List) -> lists:reverse(List).
 
 % A function concatenate/1 returns a simple list from list of lists
 
-concatenate (List) -> concatenate_pr (List,[]).
+concatenate (List) -> concatenate (List,[]).
 
-concatenate_pr ([Head | Tail], []) -> 
-	Lst = concatenate_inner (Head,[]),
-    concatenate_pr (Tail,Lst);
-
-concatenate_pr ([Head | Tail], Result) -> 
-	Lst = concatenate_inner (Head, Result),
-    concatenate_pr (Tail,Lst);
-
-concatenate_pr (_,Result) -> lists:reverse(Result).
+concatenate ([], Result) -> lists:reverse(Result);
+concatenate ([Head | Tail],Result) ->
+	Lst = concatenate_inner (Head,Result),
+	concatenate (Tail,Lst).
 
 concatenate_inner ([Head | Tail], Result) -> concatenate_inner (Tail, [Head | Result]);
-concatenate_inner (_,Result) -> Result .
+concatenate_inner ([],Result) -> Result.
+
 
 % A function flatten/1 returns a simple list from list of lists
 
